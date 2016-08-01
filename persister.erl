67cf8c7a62b537464f8d6676b01feae53f938bcf
Michał Zagórski <zagura6@gmail.com>
2016-08-01 23:46:27 +0200

    Main persister module, info logger present

--- persister.erl
+++ persister.erl
@@ -22,22 +22,25 @@ init({RowCount, SizeY, Generation}) ->
   %%%                        {register_generation, Generation}),
   {ok, State}.
 
-handle_call({register_row, X},
+handle_call({register_row, X, Pid},
             From,
             {Rows, RowCount, SizeY, Generation, Value}) ->
-  NewRows = maps:put(X, From, Rows),
-  {reply, {NewRows, RowCount, SizeY, Generation, Value}};
+  NewRows = maps:put(X, Pid, Rows),
+  error_logger:info_msg("Call register_row from ~p .~n", [Pid]),
+  {reply, ok, {NewRows, RowCount, SizeY, Generation, Value}};
 
 handle_call({write_line, X, Content},
               From,
               {Rows, 1, SizeY, Generation, Value}) ->
   NewValue = maps:put(X, Content, Value),
-
-
   gen_server:reply(From,  ok),
-  Device = io:
-  spawn_link(?MODULE, prepare_to_save, [Generation, NewValue]),
-  gen_server:stop(normal, {Rows, 0, SizeY, Generation, NewValue});
+  error_logger:info_msg("Last call of register write. ~n Save all to the file. ~n"),
+  error_logger:info_msg("Content of map: ~n ~p ~n", [NewValue]),
+  %%spawn_link(?MODULE, prepare_to_save, [Generation, NewValue]),
+  prepare_to_save(Generation, NewValue),
+  error_logger:info_msg("Finish the server."),
+  %%gen_server:stop(normal, {Rows, 0, SizeY, Generation, NewValue});
+  gen_server:stop(self());
 
 handle_call({write_line, X, Content},
             _From,
@@ -56,16 +59,23 @@ terminate(normal, _State) ->
   ok.
 
 prepare_to_save(Generation, Value) ->
-  FileName = io_lib:format("result_~p.txt", [Generation]),
-  {ok, Device} = io:open(FileName, [write]),
+  error_logger:info_msg("Call of prepare_to_save fdunction.~n"),
+  error_logger:info_msg("Args: ~p ~n ~p ~n", [Generation, Value]),
+  FileName = lists:flatten(io_lib:format("result_~p.txt", [Generation])),
+  error_logger:info_msg("FileName: ~p ~n", [FileName]),
   Rows = maps:values(Value),
+  error_logger:info_msg("Values: ~n ~p ~n", [Rows]),
+  {ok, Device} = file:open(FileName, [write]),
   try save_lines(Device, Rows)
-    after io:close(Device)
+    after file:close(Device)
   end,
+  error_logger:info_msg("Saving to file completed ~p.~n", [Generation]),
   ok.
 
 save_lines(Device, []) -> ok;
 
 save_lines(Device, [Row | Rest]) ->
-  io:fwrite("~p~n", [Row]),
+  io:fwrite(Device, "~s~n", [lists:concat(Row)]),
+  error_logger:info_msg("Next line: ~s~n", [lists:concat(Row)]),
   save_lines(Device, Rest).
+
